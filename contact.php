d030397bdd1cb5602408900bd0113d6429b2b5e0
GT1412 <81111020+GT1412@users.noreply.github.com>
2021-04-26 20:51:40 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Travel Website</title>
+</head>
+<body>
+
+<?php 
+  include 'menu.php';
+?>
+
+<section class="my-1">
+    <center><div class="py-5">
+        <h2>Contact</h2>
+    </div></center>
+</section>
+
+
+<center><b><u><h2>GT Traveller</h2></u></b>
+<b><h3>Address</h3></b>
+<p>Near Gurudeo Square</br>
+Netaji Square Babupeth</br>
+Chandrapur 442401</p>
+<b><p>Mobile Number :  9765538461</p>
+<p>Email Id      : gt@gmail.com</p></b>
+</center>
+
+</body>
+</html>
